b5cef5cc362d85b7073b21497f53b2465530204a
Mark Asfour <mj4sparrow@gmail.com>
2016-02-29 12:15:31 -0800

    improved alarm in a few more min

--- alarm.cpp
+++ alarm.cpp
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <ncurses.h>
 using namespace std;
 
 int main ()
@@ -11,26 +10,26 @@ int main ()
 	time_t timer1;
 	time_t timer;
 
+	cout << "Enter time in seconds" << endl;
+	int alarm_t = -1;
+	while (alarm_t < 0)
+		cin >> alarm_t;
+
 	time(&timer);  /* get current time; same as: timer = time(NULL)  */
 	time(&timer1);
-	cout << "set time" << endl;
-	int alarm_t;
-	cin >> alarm_t;
-
-	while (timer != timer1 + alarm_t)
+	
+	cout << "Enter CTRL + C to end alarm" << endl;
+	while (timer != timer1 + alarm_t - 1)
 	{
 		time(&timer);
-		cout << timer1 + alarm_t - timer<< endl;
+		cout << timer1 + alarm_t - timer << endl;
 		sleep(1);
 	}
 	cout << "ALARM!!!!!!" << endl;
 	for (;;)
 	{
 		cout << "BEEP" << endl;
-		char d = (char)(7);
-		printf("%c\n",d);
-		cout << '\a';
-	//	beep();
+		sleep(1);	
 	}  
 	return 0;
 }	
